b0557ca9d2de2c569f8945f8eb3efa29c9e8981d
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-10 06:49:06 +0800

    Test Ninja too

--- test.sh
+++ test.sh
@@ -12,12 +12,25 @@ mkdir -p llvm-project
 curl -fL "https://github.com/llvm/llvm-project/releases/download/llvmorg-$LLVM_VERSION/llvm-project-$LLVM_VERSION.src.tar.xz" |
   tar -C llvm-project -xJ --strip-components=1
 
-cmake -S llvm-project/llvm -B build \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX="/opt/llvm" \
-  -DLLVM_ENABLE_RUNTIMES="libcxx;openmp" \
-  -DLLVM_BUILD_LLVM_DYLIB=ON \
+if [[ -n "$USE_NINJA" ]]; then
+  mkdir -p /opt/ninja/bin
+  curl -fLO "https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-linux.zip"
+  unzip -d /opt/ninja/bin ninja-linux.zip
+  export PATH="/opt/ninja/bin:$PATH"
+  ninja --version
+fi
+
+cmake_args=(
+  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_INSTALL_PREFIX="/opt/llvm"
+  -DLLVM_ENABLE_RUNTIMES="libcxx;openmp"
+  -DLLVM_BUILD_LLVM_DYLIB=ON
   -DLLVM_LINK_LLVM_DYLIB=ON
+)
+if [[ -n "$USE_NINJA" ]]; then
+  cmake_args=(-G Ninja "${cmake_args[@]}")
+fi
+cmake -S llvm-project/llvm -B build "${cmake_args[@]}"
 cmake --build build -j "$(nproc)"
 cmake --build build --target install
 
