8f4166252b2bf02843e7436da7f04bd8cc529775
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-10 18:33:04 +0800

    Use GCC 13 in `cmake`'s subprocesses

--- test.sh
+++ test.sh
@@ -20,9 +20,9 @@ if [[ -n "$USE_NINJA" ]]; then
   ninja --version
 fi
 
+export CC="gcc-13"
+export CXX="g++-13"
 cmake_args=(
-  -DCMAKE_C_COMPILER=gcc-13
-  -DCMAKE_CXX_COMPILER=g++-13
   -DCMAKE_BUILD_TYPE=Release
   -DCMAKE_INSTALL_PREFIX="/opt/llvm"
   -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi"
